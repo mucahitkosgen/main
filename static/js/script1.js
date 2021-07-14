c74c0775dfd2807f685a9d4d68951506f6baabc4
mucahitkosgen <47633809+mucahitkosgen@users.noreply.github.com>
2021-07-14 15:56:49 +0300

    static

---
+++ static/js/script1.js
@@ -0,0 +1,20 @@
+/*==================== SHOW MENU ====================*/
+const navLeftMenu = document.getElementById('left-menu'),
+      navRightMenu = document.getElementById('right-menu'),
+      navLeftToggle = document.getElementById('nav-toggle'),
+      navRightToggle = document.getElementById('toggle-right')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navLeftToggle){
+    navLeftToggle.addEventListener('click', () =>{
+        navLeftMenu.classList.toggle('show-menu')
+    })
+}
+if(navRightToggle){
+    navRightToggle.addEventListener('click', () =>{
+        navRightMenu.classList.toggle('show-menu')
+    })
+}
+
+  
