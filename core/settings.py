2d383636be6d3c63e7ee3f20debe156c5bb9aa0c
BurcuAgdar <agdarburcu@gmail.com>
2021-07-15 12:32:29 +0300

    en

--- core/settings.py
+++ core/settings.py
@@ -154,7 +154,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'tr-Tr'
+LANGUAGE_CODE = 'en-En'
 LANGUAGES = [
     ('tr', _('Turkish')),
     ('en', _('English')),
