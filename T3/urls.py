7c9db64e2d1f8f06cd4952223d55ac17135d6cda
mucahitkosgen <47633809+mucahitkosgen@users.noreply.github.com>
2021-07-15 20:20:00 +0300

    1.1

---
+++ T3/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls.i18n import i18n_patterns
+from django.urls import path
+
+from .views import *
+
+app_name = 'T3'
+urlpatterns = [
+   # path('', main, name='main-page'),
+   # path('corporate/<slug:slug>/', corporate_page, name='corporate_page'),
+   # path('<slug:slug>/', custom_page, name='custom_page')
+]
